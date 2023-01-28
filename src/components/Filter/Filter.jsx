1b3d3e3f708a9dd3ef72ea80faead4579b8aa845
Yana Vozniuk <yana.vozniuk@ukr.net>
2023-01-28 15:17:38 +0200

    hw
    hw

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import styles from '../Filter/Filter.module.css';
+
+export default function Filter({value, onChangeFilter}) {
+    return (
+        <div className={styles.Filter}>
+            Find contacts by name
+            <input className={styles.FilterInput}
+                    type="text"
+                    value={value}
+                    onChange={(e) => onChangeFilter(e.target.value) }
+            />
+        </div>
+    );
+}
+
+
+
+Filter.propTypes = {
+    value: PropTypes.string.isRequired,
+    onChangeFilter: PropTypes.func.isRequired,
+};
